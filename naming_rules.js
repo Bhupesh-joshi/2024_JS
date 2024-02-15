181b60b850d1a630031b2cd7c458e54d8c34ecf0
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-15 23:52:42 +0530

    first commit

---
+++ naming_rules.js
@@ -0,0 +1,23 @@
+// Rules for naming Variable
+
+// var 1value = "hello";
+
+var value1 = "hello";
+console.log(value1);
+
+var _value = "hello _";
+console.log(_value);
+
+var $value = "hello $";
+console.log($value);
+
+// var first name = "John";
+
+// camel case writing
+var firstName = "John";
+console.log(firstName);
+
+// snake case writing
+var first_name = "John";
+console.log(first_name);
+
---
+++ variable.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Creating a variable
+var firstName = "BHupesh";
+console.log(firstName);
+
+// Changing the value of the variable 
+firstName = "John";
+console.log(firstName);
+
