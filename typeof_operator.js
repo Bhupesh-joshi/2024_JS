5fba0c3d181143d6f88d10e0a030353faa89162b
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-17 21:37:35 +0530

    typeof operator and Primitive data types

---
+++ typeof_operator.js
@@ -0,0 +1,44 @@
+// typeof operator
+
+// Data types (Primitive Data Types)
+
+// string
+// number
+// booleans
+// undefined
+// null
+// BigInt
+// Symbol
+
+let age = 22;
+let string = "Bhupesh";
+
+console.log(age, string);
+
+console.log(typeof age, typeof string);
+
+// Convert number to string
+age = age + "";
+// Convert string to number
+string = +string
+
+console.log(typeof age, typeof string);
+
+
+
+console.log("Second Method for coveting");
+// Second Method for coveting
+
+// string to number 
+let name = "Joshi";
+let int = 22;
+
+console.log(typeof name, typeof int); 
+
+// Converting String to number
+name = Number(name);
+
+// Converting String to number
+int = String(int);
+
+console.log(typeof name, typeof int); 
