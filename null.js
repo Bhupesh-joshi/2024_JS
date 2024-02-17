b8a648ec55416b2da9e23470063e0f1ba12cbdfd
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-18 00:07:05 +0530

    undefined done

---
+++ null.js
@@ -0,0 +1,10 @@
+// Null data type in JS
+// it is a bug in Javascript (Error in JS)
+// typeof null is object
+
+let name = null;
+console.log(name);
+console.log(typeof name);
+
+console.log(null);
+console.log(typeof null);
--- string_concatenation.js
+++ string_concatenation.js
@@ -21,3 +21,20 @@ console.log(+str1 + +str2);
 let a = +"10";
 let b = +"20";
 console.log(a + b);
+
+
+// 3
+// My name is Rohit and my age is 24
+let age = 24;
+let name = "Rohit";
+let aboutMe = "My name is" + " " + name + " " + "My age is " + age;
+
+console.log(aboutMe);
+
+
+// Template String
+let myName = "Bhupesh";
+let myAge = 20;
+
+let sec_aboutMe = `My name is ${myName} and my age is ${myAge}`
+console.log(sec_aboutMe);
