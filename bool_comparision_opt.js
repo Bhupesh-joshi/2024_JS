809f4cb8a184899f902ff320494242949afd14ff
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-18 01:13:02 +0530

    bool comparision operator done

---
+++ bool_comparision_opt.js
@@ -0,0 +1,32 @@
+// Boolean and comparison operator
+
+let num1 = 10;
+let num2 = 10;
+
+console.log(num1 > num2);
+console.log(num1 >= num2);
+
+let num3 = 20;
+let num4 = 30;
+let num5 = 20;
+let num6 = "20";
+
+// only check value(==)
+console.log(num3 == num5);
+console.log(num4 == num3);
+
+// will check also data type and value(===)
+console.log(num5 === num3);
+console.log(num5 === num6);
+
+
+
+console.log("!= vs !==");
+// != vs !==
+
+let one = 20;
+let two = "20";
+
+console.log(one != two);
+console.log(one !== two);
+
