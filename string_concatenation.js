b6805435f09f67f8fff7c7eb46ddf15a98d1d482
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-17 22:28:59 +0530

    string concatenation

---
+++ string_concatenation.js
@@ -0,0 +1,23 @@
+// String Concatenation
+// used for addition of string
+
+let string1 = "Bhupesh";
+let space = " ";
+let string2 = "Joshi";
+
+console.log(string1 + space + string2);
+
+
+// Question
+// 1
+let str1 = "22";
+let str2 = "11";
+
+console.log(str1 + str2);
+console.log(+str1 + +str2);
+
+
+// 2
+let a = +"10";
+let b = +"20";
+console.log(a + b);
