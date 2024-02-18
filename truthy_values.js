902172d6a320dbb874bbc4cfd0ab01fcb3376c5d
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-18 23:54:04 +0530

    truthy values done

---
+++ truthy_values.js
@@ -0,0 +1,40 @@
+// Truthy Values
+
+let firstName = "John";
+
+if (firstName){
+    console.log(`${firstName} - its truthy value`);
+} else {
+    console.log("FirstName is Falsy Value");
+};
+
+
+
+// 3
+let name = true;
+
+if (name){
+    console.log("True is Truthy Value");
+} else {
+    console.log("FirstName is Falsy Value");
+};
+
+
+// 4
+let num = 1;
+
+if (num){
+    console.log("1 is Truthy Value");
+} else {
+    console.log("1 is Falsy Value");
+};
+
+
+// 5
+let minus = -1;
+
+if (minus){
+    console.log("-1 is Truthy Value");
+} else {
+    console.log("-1 is Falsy Value");
+};
