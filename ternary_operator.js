c97b70181efb4e8b0c1d446c7a7d360d094377f8
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-19 00:39:08 +0530

    ternary operator done

---
+++ ternary_operator.js
@@ -0,0 +1,27 @@
+// Ternary Operator
+
+let age = 88;
+let drink;
+
+if (age >= 10) {
+    console.log(drink = "cold Drink");
+} else {
+    console.log(drink = "milk");
+}
+
+// second way
+if (age >= 10) {
+    drink = "please - Cold drink";
+} else {
+    drink = "please - Hot Milk";
+}
+console.log(drink);
+
+
+
+// Ternary Operator ** Conditional operator
+
+let myAge = 8;
+let myDrink = myAge >= 10 ? "coffee" : "water";
+console.log(myDrink);
+
