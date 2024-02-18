56dc6c6a230824474df46a541a20cbd5c2c9a75f
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-18 23:19:59 +0530

    falsy values done

---
+++ falsy_values.js
@@ -0,0 +1,62 @@
+// Falsy Values
+
+// 3
+
+let firstName = "";
+
+if(firstName){
+    console.log("FirstName");
+} else {
+    console.log("Empty Value - Falsy Value");
+}
+
+
+// 4
+
+let lastName = null;
+
+if(lastName){
+    console.log("LastName");
+}else{
+    console.log("Null Value - Falsy Value");
+}
+
+
+// 5
+
+let name;
+
+if(name){
+    console.log("Name");
+} else {
+    console.log("undefined Value - Falsy Value");
+}
+
+// 6
+let number = 0;
+
+if(number){
+    console.log("Number");
+} else {
+    console.log("0 value - Falsy Value");
+}
+
+
+// 7
+let sec_number = false;
+
+if(sec_number){
+    console.log("Number");
+} else {
+    console.log("0 value - Falsy Value");
+}
+
+
+// 8
+let the_Value = "Bhupesh";
+
+if(the_Value){
+    console.log("My name is Bhupesh");
+} else {
+    console.log("0 value - Falsy Value");
+}
---
+++ if_else.js
@@ -0,0 +1,80 @@
+// IF Else in JS
+
+let age = 18;
+
+if(age>18){
+    console.log("you can vote");
+} else {
+    console.log("you can not vote");
+};
+
+
+// 2
+let num = 4;
+if(num%2 === 0){
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+
+
+// 3
+
+let firstName = "";
+
+if(firstName){
+    console.log("FirstName");
+} else {
+    console.log("Empty Value - Falsy Value");
+}
+
+
+// 4
+
+let lastName = null;
+
+if(lastName){
+    console.log("LastName");
+}else{
+    console.log("Null Value - Falsy Value");
+}
+
+
+// 5
+
+let name;
+
+if(name){
+    console.log("Name");
+} else {
+    console.log("undefined Value - Falsy Value");
+}
+
+// 6
+let number = 0;
+
+if(number){
+    console.log("Number");
+} else {
+    console.log("0 value - Falsy Value");
+}
+
+
+// 7
+let sec_number = false;
+
+if(sec_number){
+    console.log("Number");
+} else {
+    console.log("0 value - Falsy Value");
+}
+
+
+// 8
+let the_Value = "Bhupesh";
+
+if(the_Value){
+    console.log("My name is Bhupesh");
+} else {
+    console.log("0 value - Falsy Value");
+}
