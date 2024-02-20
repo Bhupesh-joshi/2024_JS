fe9f1302c6956bee56e0b0eea1c7c42ade755319
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-21 01:20:55 +0530

    while loop done

--- else_if.js
+++ else_if.js
@@ -15,3 +15,47 @@ if(tempInDegree < 0) {
 } else {
     console.log("Too Hot Outside...!!");
 }
+
+
+
+// Example 2
+
+let myNum = 28;
+
+if (myNum < 5){
+    console.log("Less than 5");
+} else if (myNum < 10){
+    console.log("Less than 10");
+} else if (myNum < 15) {
+    console.log("Less than 15");
+} else if (myNum < 20) {
+    console.log("Less than 20");
+} else if (myNum < 25) {
+    console.log("Less than 25");
+} else {
+    console.log("Bigger Num")
+}
+
+
+
+// sunday Monday
+
+let day = 5;
+
+if (day == 1){
+    console.log("Monday");
+} else if (day == 2){
+    console.log("Tuesday");
+} else if (day == 3){
+    console.log("Wednesday");
+} else if (day == 4) {
+    console.log("Thursday")
+} else if (day == 5) {
+    console.log("Friday")
+} else if (day == 6) {
+    console.log("Saturday")
+} else if (day == 7) {
+    console.log("Sunday")
+} else {
+    console.log("Invalid Input")
+}
