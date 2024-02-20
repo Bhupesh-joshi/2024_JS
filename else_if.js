9183bc09f94020db26a55b16e27b47345c735bb3
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-21 00:29:46 +0530

    if else if done

---
+++ else_if.js
@@ -0,0 +1,17 @@
+// if else if - Multiple Conditions
+
+let tempInDegree = 0;
+
+if(tempInDegree < 0) {
+    console.log("Its Too Cold Outside");
+} else if (tempInDegree < 15) {
+    console.log("Its Cold Outside");
+} else if (tempInDegree < 25) {
+    console.log("Whether is Okay");
+} else if (tempInDegree < 35) {
+    console.log("Lets swim Today");
+} else if (tempInDegree < 45) {
+    console.log("Turn on AC");
+} else {
+    console.log("Too Hot Outside...!!");
+}
