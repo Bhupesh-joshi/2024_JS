c9c52c283960cc43e3e90bcdafc35afbf33d740f
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-23 23:27:58 +0530

    for loop again

---
+++ For_Loop.js
@@ -0,0 +1,52 @@
+// // For Loop
+
+// // Ex1
+// for(let i = 0; i<=9; i++){
+//     console.log(i);
+// }
+
+// // Ex 2
+// for (var j = 10; j <= 14; j++)
+// {
+//     console.log(j);
+// }
+
+// console.log(j);
+
+
+// // ex 3
+// let a = 0;
+// for (; a<=4; a++){
+//     console.log(a);
+// }
+
+
+// console.log("sum of 10 natural numbers");
+// // sum of 10 natural numbers
+
+// let total = 0;
+// let num = 5;
+
+// for(let i = 1; i<=num; i++){
+//     total = total + i;
+//     console.log(total);
+// }
+
+
+// ex 3
+
+// let num = 5;
+// let totalNum = 0;
+
+// for(let i = 1; i<=num; i++){
+//     totalNum = totalNum + i;
+//     console.log(totalNum);
+// }
+
+
+// Yahoo baba YT
+
+
+
+
+
