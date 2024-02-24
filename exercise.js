80f699ff38d6e7a16ed2a049ce6b439505fbe1e0
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-24 14:37:53 +0530

    Exercise - concate, slice, spread operator done

--- exercise.js
+++ exercise.js
@@ -1,9 +1,27 @@
 // Spread operator (new way)
 // add more elements in arr2
 let arr1 = ["item1", "item2"];
-let arr2 = [...arr1];
-arr2.push("item3");
+
+// slice
+// let arr2 = arr1.slice(0).concat(["item3"]);
+
+// Concat
+// let arr2 = [].concat(arr1, ["item3"]);
+
+// spread operator
+let arr2  = [...arr1, "item3"];
+
 
 console.log(arr1 === arr2);
 console.log(arr1);
 console.log(arr2);
+
+
+console.log("sum Of Arrays");
+// question 
+// How to add 2 arrays
+let array1 = ["item1", "item2"];
+let array2 = ["item3", "item4"];
+
+let sumOfArrays = [...array1, ...array2];
+console.log(sumOfArrays);
