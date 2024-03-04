9f90b82d320f044eed27e36c7ada4f9dba4aa01d
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-03-04 23:27:10 +0530

    for of loop in array

---
+++ for_of_loop_in_Array.js
@@ -0,0 +1,18 @@
+// for_of_loop_in_Array
+
+const arr = ["a", "b", "c"];
+
+for(let i of arr){
+    console.log(i);   
+}
+
+
+// ex 2
+
+const cars = ["alto", "BMW"];
+const car2 = ["AMG", "ferrari"];
+
+for(let car of cars){
+    car2.push(car);
+}
+console.log(car2);
