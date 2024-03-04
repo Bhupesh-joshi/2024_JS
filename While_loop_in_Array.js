1dd7489ef57035fe8a8d168b1d7f4bdb58602d2a
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-03-04 22:52:30 +0530

    exercise while loop in array

--- While_loop_in_Array.js
+++ While_loop_in_Array.js
@@ -1,9 +1,25 @@
 // While Loop in Array
-// const array1 = ["item1", "item2", "item3", "item4", "item5"];
 
-// let i = 1;
+const fruits = ["item1", "item2"];
 
-// while(i <= 10){
-//     i++;
-//     console.log(i);
-// }
+let j = 0;
+while(j < fruits.length){
+    console.log(fruits[j]);
+    j++;
+}
+
+
+
+
+// push new array in array
+
+const array1 = ["banana", "Orange"];
+const newArr = ["greps", "Mango"];
+
+let i = 0;
+while(i < array1.length){
+    newArr.push(array1[i]);
+    i++;
+}
+console.log(newArr);
+
