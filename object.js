552ac8d087dd9ce3004407b6a65d1eea52b6cf92
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-03-05 15:39:56 +0530

    intro to object done

---
+++ object.js
@@ -0,0 +1,21 @@
+// Objects
+
+// const person = {name : "Bhupesh", age : 20};
+// console.log(person)
+// console.log(typeof person)
+
+
+const person = {
+    name : "Bhupesh",
+    age : 22,
+    class : "MCA",
+    hobbies : ["sleeping", "YT"]
+};
+
+console.log(person.name);
+
+person.gender = "male";
+console.log(person)
+
+
+
