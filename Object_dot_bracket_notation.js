96d13c5ac5d534057c20fbfcecbd10018bceaeef
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-03-05 16:59:58 +0530

    dot and bracket notation- object done

---
+++ Object_dot_bracket_notation.js
@@ -0,0 +1,15 @@
+// Object_dot_bracket_notation.js
+
+const person = {
+    name : "Bhupesh",
+    age : 22,
+}
+
+// dot notation
+
+person.gender = "male"
+
+// bracket notation
+person["hobbies"] = "sleeping"; 
+
+console.log(person)
