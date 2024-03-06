6faee2183406574790081794f4b4a2c128bed361
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-03-06 23:48:31 +0530

    Object Destructuring - shortcut method

--- Object_destructuring.js
+++ Object_destructuring.js
@@ -1,11 +1,22 @@
 // Object Destructuring
 
+// const carModel = {
+//     car1 : "alto",
+//     car2 : "BMW"
+// }
+
+// const car1 = carModel.car1;
+// const car2 = carModel.car2;
+// console.log(car1, car2);
+
+
+
+
+// Shortcut Method
 const carModel = {
     car1 : "alto",
     car2 : "BMW"
 }
 
-const car1 = carModel.car1;
-const car2 = carModel.car2;
-
+const {car1, car2} = carModel;
 console.log(car1, car2);
