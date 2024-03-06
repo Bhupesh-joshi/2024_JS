49611f4ae50628cf639880483116c56ff24b69c2
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-03-06 23:50:40 +0530

    Object Destructuring - Changing the key name

--- Object_destructuring.js
+++ Object_destructuring.js
@@ -13,10 +13,23 @@
 
 
 // Shortcut Method
+// const carModel = {
+//     car1 : "alto",
+//     car2 : "BMW"
+// }
+
+// const {car1, car2} = carModel;
+// console.log(car1, car2);
+
+
+
+
+// Changing the key name
+
 const carModel = {
     car1 : "alto",
     car2 : "BMW"
 }
 
-const {car1, car2} = carModel;
-console.log(car1, car2);
+const {car1:model1, car2:model2} = carModel;
+console.log(model1, model2);
