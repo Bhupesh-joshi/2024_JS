9cb56a9f4c777d7d46851c465581643336d9ab84
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-03-06 23:11:23 +0530

    spread operator in array and object exercise done

--- spread_operator_Object.js
+++ spread_operator_Object.js
@@ -1,3 +1,36 @@
 // Spread Operator in Object
 
+// Array
+
+// const array1 = [1, 2, 3];
+// const array2 = [4, 5, 6];
+
+// const newArray = [...array1, ...array2]
+// const againNewArray = [..."1234567"]
+
+// console.log(newArray)
+// console.log(againNewArray)
+
+
+
+// Object
+
+const object1 = {
+    key1 : "value1",
+    key2 : "value2"
+}
+
+const object2 = {
+    key3 : "value3",
+    key4 : "value4"
+}
+
+const newObject = {...object1, ...object2, name : "Bhupesh"};
+console.log(newObject)
+
+const myObj = {..."abc"};
+console.log(myObj);
+
+const alphabet = {..."abcdefghighijklmnopqrstuvwxyz"};
+console.log(alphabet)
 
