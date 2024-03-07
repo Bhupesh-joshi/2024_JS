87b6cd367a569f49da60419c7b7c7ca33411d8fc
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-03-07 22:57:06 +0530

    function expression

--- functionExpression.js
+++ functionExpression.js
@@ -1,1 +1,51 @@
 // Function Expression
+
+const HBD = function(){
+    console.log("Happy Birthday");
+}
+HBD();
+
+
+
+// Add two nums
+
+const addTwo = function(num1, num2){
+    return num1 + num2;
+}
+
+console.log(addTwo(2,2));
+
+
+
+// isEven
+
+const isEven = function(num){
+    return num % 2 === 0;
+}
+
+console.log(isEven(45));
+
+
+
+// firstChar
+const firstChar = function(string){
+    return string[0];
+}
+
+console.log(firstChar("Bhupesh"));
+
+
+
+// Target
+const isTarget = function(array, target){
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+const myArray = [1,2,3,4,5];
+const ans = isTarget(myArray, 3);
+console.log(ans);
