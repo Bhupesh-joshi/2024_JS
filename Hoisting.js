2fa3514ba0b71e865919998134deb72a013ab9ab
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-03-07 23:42:15 +0530

    hoisting

---
+++ Hoisting.js
@@ -0,0 +1,14 @@
+// Hoisting
+hello();
+function hello() {
+    console.log("Hello Bhupesh");
+}
+
+// console.log(hello);
+// var hello = "Hello Bhupesh";
+
+// console.log(hello);
+// let hello = "Hello Bhupesh";
+
+// console.log(hello);
+// let hello = "Hello Bhupesh";
