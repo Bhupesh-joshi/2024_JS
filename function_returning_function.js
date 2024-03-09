fc641d47aa35b6bc5a025a5c7cf2770b2953de36
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-03-09 23:36:58 +0530

    forEach Method

--- function_returning_function.js
+++ function_returning_function.js
@@ -1,2 +1,12 @@
 // function_returning_function.js
 
+function myFunc(){
+    function myFunc2(){
+        return "inside myFunc2";
+    };
+
+    return myFunc2;
+};
+
+const result = myFunc();
+console.log(result());
