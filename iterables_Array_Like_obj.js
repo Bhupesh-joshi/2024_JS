3c85c9f0aa2443ad16e8914c2f4e92064c89c696
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-03-12 23:54:46 +0530

    iterables and array like objects

---
+++ iterables_Array_Like_obj.js
@@ -0,0 +1,28 @@
+// iterables
+
+// String
+const myStr = "Bhupesh Joshi";
+
+for(let char of myStr){
+    console.log(char);
+}
+
+
+
+// Array
+
+const myArray = ["item1", "item2", "item3"];
+
+for(let items of myArray){
+    console.log(items);
+}
+
+
+
+
+// Array like Objects
+
+const newString = "hello";
+
+console.log(newString.length, newString);
+console.log(newString[1], newString);
