90a90b0dbe7c65d72f8e36831699e024c3f62ac4
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-03-28 23:48:23 +0530

    getter

--- class_keyword.js
+++ class_keyword.js
@@ -1,1 +1,52 @@
 console.log("class_keyword");
+
+class CreateUser {
+    constructor(firstName, lastName, email, age, address){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+        this.address = address;
+    };
+
+    about(){
+        return `${this.firstName} is ${this.age} years old`;
+    };
+    is18(){
+        return this.age >= 18;
+    };
+    singh(){
+        return "singh Called";
+    };
+
+}
+
+
+function CreateUser(firstName, lastName, email, age, address){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.age = age;
+    this.address = address;
+};
+
+CreateUser.prototype.about = function() {
+            return `${this.firstName} is ${this.age} years old`;
+        };
+CreateUser.prototype.is18 = function() {
+            return this.age >= 18;
+        };
+CreateUser.prototype.singh = function() {
+            return "singh Called";
+        };
+
+const user1 = new CreateUser("Bhupesh", "Joshi", "bhupeshjoshi@gmail.com", 23, "123 st SJ, CA");
+const user2 = new CreateUser("Ellis", "perry", "bhupeshjoshi@gmail.com", 23, "123 st NYC, NY");
+const user3 = new CreateUser("Shreyanka", "Patil", "bhupeshjoshi@gmail.com", 23, "123 Nagpur Mumbai");
+
+for(let key in user1){
+    // console.log(key);
+    if(user1.hasOwnProperty(key)){
+        console.log(key);
+    }
+}
--- more_about_prototype.js
+++ more_about_prototype.js
@@ -1,2 +1,13 @@
 console.log("More about Prototype");
 
+// JS internally creating Array like this
+
+let numbers = new Array(1,2,3);
+console.log(Array.prototype);
+console.log(numbers);
+
+
+// we create array like this
+
+let nums = [1,2,3,4];
+
