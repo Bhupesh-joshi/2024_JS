234a8ef7af0eb1380789b9b324dec8099c41ae5a
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-04-11 23:33:36 +0530

    changing style of element

--- part2-dom_manupulation/DOM/get-and-set-attribute.js
+++ part2-dom_manupulation/DOM/get-and-set-attribute.js
@@ -3,8 +3,8 @@ const link = document.querySelector("a");
 console.log(link.getAttribute("href"));
 
 // setAttribute
-link.setAttribute("href", "https://bhupesh.com")
-// console.log(link.getAttribute("href"));
+// link.setAttribute("href", "https://bhupesh.com")
+console.log(link.getAttribute("href"));
 
 
 const inputElement = document.querySelector(".form-todo input");
