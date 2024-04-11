aaf15129592d52841c847cf0f7e2bcd63c0f1122
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-04-11 23:32:37 +0530

    innerText

--- part2-dom_manupulation/DOM/get-and-set-attribute.js
+++ part2-dom_manupulation/DOM/get-and-set-attribute.js
@@ -4,8 +4,8 @@ console.log(link.getAttribute("href"));
 
 // setAttribute
 link.setAttribute("href", "https://bhupesh.com")
-console.log(link.getAttribute("href"));
+// console.log(link.getAttribute("href"));
 
 
-// const inputElement = document.querySelector(".form-todo input");
+const inputElement = document.querySelector(".form-todo input");
 console.log(inputElement.getAttribute("type"));
