3b71bfef324dc1e2bf7f5c6318beec22ff580c17
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-04-13 22:52:46 +0530

    classList

--- part2-dom_manupulation/DOM/iterableElement.js
+++ part2-dom_manupulation/DOM/iterableElement.js
@@ -1,5 +1,29 @@
-const navItems = document.querySelectorAll(".nav-item");
-console.log(navItems[0]);
-console
+let navItems = document.getElementsByTagName("a");
+// console.log(navItems[0]);
+
+// for(let i=0; i<navItems.length; i++){
+//     // console.log(navItems[i]);
+//     const navItem = navItems[i];
+//     navItem.style.backgroundColor = "#FFF"
+//     navItem.style.color = "#000"
+//     navItem.style.fontWeight = "bold"
+// };
+
+// for(let navItem of navItems){
+//     navItem.style.backgroundColor = "#FFF"
+//     navItem.style.color = "#000"
+//     navItem.style.fontWeight = "bold"
+// };
 
 
+
+
+// HTML collection changing into Array
+navItems = Array.from(navItems);
+
+// forEach
+navItems.forEach((navItem)=> {
+    navItem.style.backgroundColor = "#FFF"
+    navItem.style.color = "#000"
+    navItem.style.fontWeight = "bold"
+})
