d7b8ab5e68fd75a70b48b88f1338efa6b59ea5cb
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-04-15 23:29:46 +0530

    insert adjacent HTML

--- part2-dom_manupulation/DOM/innerHTML.js
+++ part2-dom_manupulation/DOM/innerHTML.js
@@ -4,4 +4,4 @@ const headLine = document.querySelector(".headline");
 // console.log(headLine.innerHTML);
 headLine.innerHTML = "<h1>Inner HTML has changed</h1>";
 headLine.innerHTML += "<button class=\"btn\"> Learn More </button>"
-console.log(headLine.innerHTML);
+// console.log(headLine.innerHTML);
