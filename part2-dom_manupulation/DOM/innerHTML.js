83fc381e64af5b1175b2a1467f15bd5790775368
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-04-15 23:30:14 +0530

    clone nodes

--- part2-dom_manupulation/DOM/innerHTML.js
+++ part2-dom_manupulation/DOM/innerHTML.js
@@ -3,5 +3,5 @@
 const headLine = document.querySelector(".headline");
 // console.log(headLine.innerHTML);
 headLine.innerHTML = "<h1>Inner HTML has changed</h1>";
-headLine.innerHTML += "<button class=\"btn\"> Learn More </button>"
-// console.log(headLine.innerHTML);
+// headLine.innerHTML += "<button class=\"btn\"> Learn More </button>"
+console.log(headLine.innerHTML);
