ffdef0164c4ebb3b9ad5c283df4d3552a633ce92
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-04-17 23:53:57 +0530

    replace child

---
+++ part2-dom_manupulation/DOM/DOM-Tree.js
@@ -0,0 +1,20 @@
+const rootNode = document.getRootNode();
+const htmlElementNode = rootNode.childNodes[0];
+// console.dir(htmlElementNode);
+// console.log(htmlElementNode.childNodes); NodeList(3) [head, text, body]
+
+// child relationship
+const headElementNode = htmlElementNode.childNodes[0];
+const textNode1 = htmlElementNode.childNodes[1];
+const bodyElementNode = htmlElementNode.childNodes[2];
+console.log(headElementNode);
+console.log(textNode1);
+console.log(bodyElementNode);
+
+// parent relationship
+console.log(headElementNode.parentNode);
+
+// sibling relationship
+console.log(headElementNode.nextSibling);
+console.log(headElementNode.nextSibling.nextSibling);
+console.log(headElementNode.nextSibling.textContent);
